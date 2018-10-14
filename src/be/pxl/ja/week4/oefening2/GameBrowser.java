00a084e77cbdc143d8617c303084c23ce58f9a0f
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 22:15:35 +0200

    simplify lambda, if and return statement.

--- src/be/pxl/ja/week4/oefening2/GameBrowser.java
+++ src/be/pxl/ja/week4/oefening2/GameBrowser.java
@@ -13,4 +13,37 @@ public class GameBrowser {
     public List<VideoGame> showGamesForSearch(String search) {
         return gameCollection.selectGames(videoGame -> videoGame.getName().toLowerCase().contains(search.toLowerCase()));
     }
+
+//    public List<VideoGame> showFreeGames() {
+//        return gameCollection.selectGames(new Predicate<VideoGame>() {
+//            @Override
+//            public boolean test(VideoGame videoGame) {
+//                if (videoGame.getPrice() == 0.0) {
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+
+
+//    public List<VideoGame> showFreeGames() {
+//        return gameCollection.selectGames(videoGame -> {
+//            if (videoGame.getPrice() == 0.0) {
+//                return true;
+//            }
+//            return false;
+//        });
+//    }
+
+//    public List<VideoGame> showFreeGames() {
+//        return gameCollection.selectGames(videoGame -> {
+//            return videoGame.getPrice() == 0.0;
+//        });
+//    }
+
+    public List<VideoGame> showFreeGames() {
+        return gameCollection.selectGames(videoGame -> videoGame.getPrice() == 0.0);
+    }
+
 }
