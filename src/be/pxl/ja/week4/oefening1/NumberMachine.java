4e80f3b5108c0699374916af3de90366782dff0c
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 21:09:37 +0200

    create converNumbers that takes a converter ("Standard Func. Interface" - function that takes an Integer and returns a String) as input and applies it on all numbers.

--- src/be/pxl/ja/week4/oefening1/NumberMachine.java
+++ src/be/pxl/ja/week4/oefening1/NumberMachine.java
@@ -1,25 +1,39 @@
 package be.pxl.ja.week4.oefening1;
 
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class NumberMachine {
-	private ArrayList<Integer> numbers = new ArrayList<>();
+    private ArrayList<Integer> numbers = new ArrayList<>();
 
-	public NumberMachine(int[] numbers) {
-		for(int i=0;i<numbers.length;i++)
-			this.numbers.add(numbers[i]);
-	}
+    public NumberMachine(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++)
+            this.numbers.add(numbers[i]);
+    }
 
-	public String processNumbers(NumberFilter filter) {
-		String result = "";
-		for(int i=0;i<numbers.size();i++) {
-			if(filter.check(numbers.get(i))) {
-				if(!result.equals("")) {
-					result += "-";
-				}
-				result += numbers.get(i);
-			}
-		}
-		return result;
-	}
+    public String processNumbers(NumberFilter filter) {
+        String result = "";
+        for (int i = 0; i < numbers.size(); i++) {
+            if (filter.check(numbers.get(i))) {
+                if (!result.equals("")) {
+                    result += "-";
+                }
+                result += numbers.get(i);
+            }
+        }
+        return result;
+    }
+
+    public String convertNumbers(Function<Integer, String> converter) {
+        String result = "";
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!result.equals("")) {
+                result += "-";
+            }
+            result += converter.apply(numbers.get(i));
+        }
+
+        return result;
+
+    }
 }
