2bf77453c270804347d014ff49e7959f7eaa9825
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 21:49:27 +0200

    create class GameBrowser that has a GameCollection

---
+++ src/be/pxl/ja/week4/oefening2/GameBrowser.java
@@ -0,0 +1,9 @@
+package be.pxl.ja.week4.oefening2;
+
+public class GameBrowser {
+    private GameCollection gameCollection;
+
+    public GameBrowser(GameCollection gameCollection) {
+        this.gameCollection = gameCollection;
+    }
+}
