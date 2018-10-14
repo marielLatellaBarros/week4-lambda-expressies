2abc53cb7b45bfd02bbba1bf98aa81db33cc99bc
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 21:47:48 +0200

    add method selectGames that returns a new list of VideoGames depending on a boolean condition. first made an iteration to add games to the array list. then resolved the Standard Interface that will check the parameter => Predicate Function the Predicate has a test method that returns a boolean based on the videoGame. If condition is met, the new game list gets filled.

--- src/be/pxl/ja/week4/oefening2/GameCollection.java
+++ src/be/pxl/ja/week4/oefening2/GameCollection.java
@@ -1,12 +1,25 @@
 package be.pxl.ja.week4.oefening2;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class GameCollection {
     private ArrayList<VideoGame> videoGames = new ArrayList<>();
 
 
-    public void addGame (VideoGame videogame) {
+    public void addGame(VideoGame videogame) {
         videoGames.add(videogame);
+    }
+
+    //it's better to use a List as a return type instead of ArrayList because the scope is bigger.
+    public List<VideoGame> selectGames(Predicate<VideoGame> gameChecker) {
+        List<VideoGame> result = new ArrayList<>();
+        for (VideoGame videoGame : videoGames) {
+            if (gameChecker.test(videoGame)) {
+                result.add(videoGame);
+            }
+        }
+        return result;
     }
 }
