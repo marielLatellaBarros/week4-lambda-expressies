6afa5f623e63130ac3a9b276e2ae5b3cd0828901
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 22:37:55 +0200

    add showGamesInGenre. It returns a list of videoGames where the genre matches a specific given genre. Iterate through all genres and compare (lowerCase). simplify lambda.

--- src/be/pxl/ja/week4/oefening2/GameBrowser.java
+++ src/be/pxl/ja/week4/oefening2/GameBrowser.java
@@ -1,5 +1,6 @@
 package be.pxl.ja.week4.oefening2;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -46,4 +47,28 @@ public class GameBrowser {
         return gameCollection.selectGames(videoGame -> videoGame.getPrice() == 0.0);
     }
 
+//    public List<VideoGame> showGamesInGenre(String genre) {
+//        return gameCollection.selectGames(new Predicate<VideoGame>() {
+//            @Override
+//            public boolean test(VideoGame videoGame) {
+//                for (String genreGame : videoGame.getGenres()) {
+//                    if (genreGame.toLowerCase().equalsIgnoreCase(genre)) {
+//                        return true;
+//                    }
+//                }
+//                return false;
+//            }
+//        });
+//    }
+
+    public List<VideoGame> showGamesInGenre(String genre) {
+        return gameCollection.selectGames(videoGame -> {
+            for (String genreGame : videoGame.getGenres()) {
+                if (genreGame.toLowerCase().equalsIgnoreCase(genre)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
 }
