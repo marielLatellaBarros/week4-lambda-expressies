f310c7bbcb7cd2b94185b6d29cd107c169454631
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-14 22:04:19 +0200

    add method showGamesForSearch that returns a new list of videoGames using a search parameter to match the valid results.

--- src/be/pxl/ja/week4/oefening2/GameBrowser.java
+++ src/be/pxl/ja/week4/oefening2/GameBrowser.java
@@ -1,9 +1,24 @@
 package be.pxl.ja.week4.oefening2;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class GameBrowser {
     private GameCollection gameCollection;
 
     public GameBrowser(GameCollection gameCollection) {
         this.gameCollection = gameCollection;
+    }
+
+    public List<VideoGame> showGamesForSearch(String search) {
+        return gameCollection.selectGames(new Predicate<VideoGame>() {
+            @Override
+            public boolean test(VideoGame videoGame) {
+                if (videoGame.getName().toLowerCase().contains(search.toLowerCase())) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
